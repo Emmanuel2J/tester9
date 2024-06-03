6aba9cf5c27aa38d7deda8fbf841fd9bff26701d
ABUzakep <nishadnisha2001@gmail.com>
2024-06-03 12:59:07 +0530

    notification

--- app/src/main/java/com/example/tester/home.java
+++ app/src/main/java/com/example/tester/home.java
@@ -10,7 +10,6 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
